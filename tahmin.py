475fcb17d5b2dfb11711beeb0163c3c9a749c73f
ynsemr46 <yunusemrekarabayir29@gmail.com>
2025-06-04 20:10:22 +0300

    Gerekli değişiklikler yapıldı

--- tahmin.py
+++ tahmin.py
@@ -879,9 +879,3 @@ def index():
         days=days,
         view=view
     )
-
-if __name__ == '__main__':
-    import os
-    os.makedirs('static', exist_ok=True)
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
